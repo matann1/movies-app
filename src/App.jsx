49a0d1da83315da5b524ad725b366f24b886cef7
Matan <48412348+matann1@users.noreply.github.com>
2025-02-24 13:48:49 +0200

    add a movie card component and implement search

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import MovieCard from "./components/MovieCard";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
+import { useDebounce } from "react-use";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -19,12 +21,17 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
-  const fetchMovies = async () => {
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=populariry.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=populariry.desc`;
       const res = await fetch(endpoint, API_OPTIONS);
 
       if (!res.ok) {
@@ -48,8 +55,8 @@ const App = () => {
     }
   };
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return (
     <main>
@@ -73,9 +80,7 @@ const App = () => {
             ) : (
               <ul>
                 {movies.map((movie) => (
-                  <p key={movie.id} className="text-white">
-                    {movie.title}
-                  </p>
+                  <MovieCard key={movie.id} movie={movie} />
                 ))}
               </ul>
             )}
